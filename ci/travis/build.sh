18976309286722affd74f46f466d6d0c702f1437
Juan Rodriguez Hortala <hortala@amazon.com>
2019-05-29 18:21:25 -0700

    Use a default value for all docker env vars
    Signed-off-by: Juan Rodriguez Hortala <hortala@amazon.com>

--- ci/travis/build.sh
+++ ci/travis/build.sh
@@ -5,13 +5,13 @@ docker pull osrf/ros2:nightly
 docker run -it --rm -v "${TRAVIS_BUILD_DIR}/..:/opt/workspace/src" \
     --network host --name=osrf_ros2_nightly --privileged osrf/ros2:nightly \
     -e SCRIPT="${SCRIPT}" \
-    -e MSG_TYPES="${MSG_TYPES}" \
-    -e MAX_PUBLISHERS="${MAX_PUBLISHERS}" \
-    -e MAX_SUBSCRIBERS="${MAX_SUBSCRIBERS}" \
-    -e PUBLISH_FREQUENCIES="${PUBLISH_FREQUENCIES}" \
-    -e DURATION="${DURATION}" \
-    -e NUM_EXPERIMENTS="${NUM_EXPERIMENTS}" \
-    -e MAX_SERVICES="${MAX_SERVICES}" \
-    -e MAX_CLIENTS="${MAX_CLIENTS}" \
-    -e REQUEST_FREQUENCIES="${REQUEST_FREQUENCIES}" \
+    -e MSG_TYPES="${MSG_TYPES:-nil}" \
+    -e MAX_PUBLISHERS="${MAX_PUBLISHERS:-nil}" \
+    -e MAX_SUBSCRIBERS="${MAX_SUBSCRIBERS:-nil}" \
+    -e PUBLISH_FREQUENCIES="${PUBLISH_FREQUENCIES:-nil}" \
+    -e DURATION="${DURATION:-nil}" \
+    -e NUM_EXPERIMENTS="${NUM_EXPERIMENTS:-nil}" \
+    -e MAX_SERVICES="${MAX_SERVICES:-nil}" \
+    -e MAX_CLIENTS="${MAX_CLIENTS:-nil}" \
+    -e REQUEST_FREQUENCIES="${REQUEST_FREQUENCIES:-nil}" \
     /bin/bash -c "/opt/workspace/src/$(basename ${TRAVIS_BUILD_DIR})/ci/travis/docker_build.sh"
