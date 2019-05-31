6a385ebc24a07e9c323d9e1dd4515202f9204ced
Juan Rodriguez Hortala <hortala@amazon.com>
2019-05-31 16:38:34 -0700

    Adapt index format to benchmark results
    Signed-off-by: Juan Rodriguez Hortala <hortala@amazon.com>

--- ci/travis/docker_build.sh
+++ ci/travis/docker_build.sh
@@ -6,6 +6,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 function initialize_environment {
     apt-get update
     source /opt/ros/dashing/setup.bash
+    pip3 install 'jinja2>=2.10' --user
 
     export ROS2_SDK_INSTALL_PATH="/opt/ros/dashing"
     export ROS2_PERFORMANCE_TEST_INSTALL_PATH="/opt/workspace/install/"
@@ -37,91 +38,69 @@ function setup_ros2_performance {
     echo "Setting up ros2-performance"
     echo "WORKAROUND: disabling wait_for_discovery for performance experiments until is is supported by RMW DPS"
     sed -i 's@this->wait_discovery@//this->wait_discovery@g' src/ros2-performance/performances/performance_test/src/ros2/system.cpp
-    colcon build --packages-up-to performance_test_msgs performance_test --merge-install
+    colcon build --packages-up-to performance_test benchmark --merge-install
     echo "Done setting up ros2-performance"
 }
 
-function run_experiments {
-    # Using (mostly) default values, TODO setup suite more carefully
-    
-    function experiment_setup1 {
-        export SCRIPT=pub_sub_ros2.sh
-        export MSG_TYPES=10b 
-        export MAX_PUBLISHERS=1
-        export MAX_SUBSCRIBERS=5
-        export PUBLISH_FREQUENCIES=100
-        export DURATION=10
-        export NUM_EXPERIMENTS=2
-    }
+function run_benchmarks {
+    TEST_DURATION=60
+    IPC='on'
+    BENCHMARK_IDS='sierra_nevada mont_blanc'
 
-    function experiment_setup2 {
-        export SCRIPT=pub_sub_separate_process.sh
-        export MSG_TYPES=10b 
-        export MAX_PUBLISHERS=1 
-        export MAX_SUBSCRIBERS=5
-        export PUBLISH_FREQUENCIES=100 
-        export DURATION=10
-        export NUM_EXPERIMENTS=2
-    }
+    source install/local_setup.bash
+    pushd install/lib/benchmark
+    git clone --branch=${GH_PAGES_BRANCH} https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${GH_PAGES_BRANCH}
+    PERFORMANCES_ROOT="$(pwd)/../../../src/ros2-performance/performances"
 
-    function experiment_setup3 {
-        export SCRIPT=client_service_ros2.sh 
-        export MAX_SERVICES=1 
-        export MAX_CLIENTS=5 
-        export REQUEST_FREQUENCIES=100
-        export DURATION=10
-        export NUM_EXPERIMENTS=2
-    }
-
-    function experiment_setup4 {
-        export SCRIPT=only_subs.sh
-        export MSG_TYPES=10b
-        export MAX_PUBLISHERS=1
-        export MAX_SUBSCRIBERS=5
-        export DURATION=10
-        export NUM_EXPERIMENTS=2
-    }
-
-    EXPERIMENT_SETUPS="experiment_setup1 experiment_setup2 experiment_setup3 experiment_setup4"
-    echo "Running experiments"
-    pushd src/ros2-performance/performances/performance_test
-    source env.sh
-    for EXPERIMENT_SETUP in ${EXPERIMENT_SETUPS}
+    for BENCHMARK_ID in ${BENCHMARK_IDS}
     do
-        set -x
-        ${EXPERIMENT_SETUP}
-        set +x
-        RMW_IMPLEMENTATION=rmw_dps_cpp bash -x "scripts/${SCRIPT}"
-    done    
+        ${BENCHMARK_SETUP}
+        echo "--------------------------------------------------"
+        echo "Running benchmark [${BENCHMARK_ID}]"
+        echo "--------------------------------------------------"
+        ./benchmark "topology/${BENCHMARK_ID}.json" -t ${TEST_DURATION} --ipc ${IPC}
+        echo "Done running benchmark [${BENCHMARK_ID}]"
+        echo
+
+        RESULTS_ID="${CURRENT_DATE_FORMAT}/${BENCHMARK_ID}"
+        RESULTS_ROOT="${GH_PAGES_BRANCH}/results/${RESULTS_ID}"
+        mkdir -p "${RESULTS_ROOT}"
+        # copy results data 
+        tar cvzf "${RESULTS_ROOT}/log.tgz" log
+        # generate plots
+        mkdir -p "${RESULTS_ROOT}/plots"
+        python3 "${PERFORMANCES_ROOT}/performance_test/scripts/plot_scripts/benchmark_app_evaluation.py" \
+            --target "${PERFORMANCES_ROOT}/benchmark/performance_target/sierra_nevada_rpi3.json" \
+            --resources "log/resources.txt" \
+            --latency "log/latency_total.txt" \
+            --plot-filename "${RESULTS_ROOT}/plots/benchmark_app_evaluation.svg"
+        echo "generated ${RESULTS_ROOT}/plots/benchmark_app_evaluation.svg"
+        python3 "${PERFORMANCES_ROOT}/performance_test/scripts/plot_scripts/cpu_ram_plot.py" \
+            "log/resources.txt" \
+            --target "${PERFORMANCES_ROOT}/benchmark/performance_target/sierra_nevada_rpi3.json" \
+            --x time --y cpu --y2 rss \
+            --plot-filename "${RESULTS_ROOT}/plots/cpu_ram_plot.svg"
+        echo "generated ${RESULTS_ROOT}/plots/cpu_ram_plot.svg"
+        echo
+    done
     popd
     echo "Done running experiments"
 }
 
-function publish_experiments_results {
-    # FIXME: this is broken until adapted to running multiple experiments on run_experiments
+function publish_benchmark_results {
+    pushd "install/lib/benchmark/${GH_PAGES_BRANCH}"
 
-    echo "Publishing experiments results"
-    pip3 install jinja2>=2.10 --user
-
-    pushd src/ros2-performance
-    git clone --branch=${GH_PAGES_BRANCH} https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${GH_PAGES_BRANCH}
-    SCRIPT_NAME="${SCRIPT%.*}"
-    RESULTS_ID="${CURRENT_DATE_FORMAT}-${SCRIPT_NAME}"
-    RESULTS_ROOT="${GH_PAGES_BRANCH}/results/${RESULTS_ID}"
-    mkdir -p "${RESULTS_ROOT}"
-    cp performances/performance_test/results/*/* ${RESULTS_ROOT}
+    # Upload the results for all benchmarks run on this job
+    RESULTS_ID="${CURRENT_DATE_FORMAT}"
+    RESULTS_ROOT="results/${RESULTS_ID}"
+    echo "Publishing benchmark results for [${RESULTS_ID}]"
     python3 "${SCRIPT_DIR}/write_results_index.py" "${RESULTS_ROOT}"
-    pushd ${GH_PAGES_BRANCH}
-    echo "- [Experiment ${RESULTS_ID}](results/${RESULTS_ID}) for [Travis build ${TRAVIS_BUILD_NUMBER}](${TRAVIS_BUILD_WEB_URL}) and [Travis job ${TRAVIS_JOB_NUMBER}](${TRAVIS_JOB_WEB_URL})" >> README.md
+    echo "- [Benchmark ${RESULTS_ID}](results/${RESULTS_ID}) for [Travis build ${TRAVIS_BUILD_NUMBER}](${TRAVIS_BUILD_WEB_URL}) and [Travis job ${TRAVIS_JOB_NUMBER}](${TRAVIS_JOB_WEB_URL})" >> README.md
     git add .
     git commit -m "Upload ${RESULTS_ROOT} for Travis build ${TRAVIS_BUILD_NUMBER} and job ${TRAVIS_JOB_NUMBER}"
     git push origin ${GH_PAGES_BRANCH}
     popd
-
-    # TODO build plots
-    # use PLOT_FILENAME to enable plotting to file
-    popd
-    echo "Done publishing experiments results"
+    echo "Done publishing benchmark results for [${RESULTS_ID}]"
 }
 
 initialize_environment
@@ -131,6 +110,6 @@ mkdir -p /opt/workspace/src
 pushd /opt/workspace
 setup_rmw_dps
 setup_ros2_performance
-run_experiments
-publish_experiments_results
+run_benchmarks
+publish_benchmark_results
 popd
--- ci/travis/write_results_index.py
+++ ci/travis/write_results_index.py
@@ -1,6 +1,11 @@
 from jinja2 import Template
 from pathlib import Path
 
+_plot_width = {
+  'benchmark_app_evaluation.svg': 1200,
+  'cpu_ram_plot.svg': 1000
+}
+
 _results_index_template = Template('''
 <!DOCTYPE html>
 <html>
@@ -8,15 +13,22 @@ _results_index_template = Template('''
 </head>
 <body>
 
-<h2>Results</h2>
+<h2>Results for benchmark run {{result_id}}</h2>
 
-<ul>
-  {% for result_file in result_files %}
-  <li>
-    <a href="{{result_file.name}}">{{result_file.name}}</a>
-  </li>
+{% for benchmark in benchmarks %}
+  <h3>{{benchmark}}</h3>
+
+  <div align="left">
+    <a href="{{benchmark}}/log.tgz">log.tgz</a>
+  </div>
+
+  {% for plot in plots %}
+  <div align="left">
+    <img src="{{benchmark}}/plots/{{plot}}" alt="{{plot}}" width="{{plot_width[plot]}}">
+  </div>
   {% endfor %}
-</ul> 
+  
+{% endfor %}
 
 </body>
 </html>
@@ -24,8 +36,14 @@ _results_index_template = Template('''
 
 if __name__ == '__main__':
     import sys
-    results_root = sys.argv[1]
-    index_path = Path(results_root) / 'index.html'
+    root_path = Path(sys.argv[1])
+    benchmarks_paths = [ p for p in root_path.iterdir() if p.is_dir() ]
+    index_path = root_path / 'index.html'
     with open(str(index_path), 'w') as out_f:
-        rendered_index = _results_index_template.render(result_files=Path(results_root).glob('*.csv'))
+        rendered_index = _results_index_template.render(
+          result_id=root_path.name,
+          benchmarks=[ p.name for p in benchmarks_paths ],
+          plots=[ p.name for p in (benchmarks_paths[0] / 'plots').iterdir() if p.is_file() ],
+          plot_width=_plot_width
+        )
         out_f.write(rendered_index)
