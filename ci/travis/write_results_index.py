133b417f07d835d123a1e5f84957739836c3431a
Juan Rodriguez Hortala <hortala@amazon.com>
2019-06-06 13:52:09 -0700

    Run benchmark for both DPS and CycloneDDS
    Signed-off-by: Juan Rodriguez Hortala <hortala@amazon.com>

--- ci/travis/write_results_index.py
+++ ci/travis/write_results_index.py
@@ -14,45 +14,55 @@ _results_index_template = Template('''
 </head>
 <body>
 
-<h2>Results for benchmark run {{result_id}} (<a href="{{travis_build_url}}">Travis build {{travis_build_number}}</a>, <a href="{{travis_job_url}}">Travis job {{travis_job_number}}</a>)</h2>
+<h1>Results for benchmark run {{result_id}} (<a href="{{travis_build_url}}">Travis build {{travis_build_number}}</a>, <a href="{{travis_job_url}}">Travis job {{travis_job_number}}</a>)</h1>
 
-{% for benchmark in benchmarks %}
-  <h3>{{benchmark.name}} (exit code={{benchmark.exit_code}})</h3>
+{% for rmw_impl, benchmarks in rwm_benchmarks %}
+  <h2>{{rmw_impl}} </h2>
 
-  <div align="left">
-    <a href="{{benchmark.name}}/log.tgz">log.tgz</a>
-  </div>
+  {% for benchmark in benchmarks %}
+    <h3>{{benchmark.name}} (exit code=<font color="{{benchmark.color}}">{{benchmark.exit_code}}</font>)</h3>
 
-  {% for plot in plots %}
-  <div align="left">
-    <img src="{{benchmark.name}}/plots/{{plot}}" alt="{{plot}}" width="{{plot_width[plot]}}">
-  </div>
+    <div align="left">
+      <a href="{{rmw_impl}}/{{benchmark.name}}/log.tgz">log.tgz</a>
+    </div>
+
+    {% for plot in plots %}
+    <div align="left">
+      <img src="{{rmw_impl}}/{{benchmark.name}}/plots/{{plot}}" alt="{{plot}}" width="{{plot_width[plot]}}">
+    </div>
+    {% endfor %}
+
   {% endfor %}
-  
+
 {% endfor %}
 
 </body>
 </html>
 ''')
 
-BenchmarkInfo = namedtuple('BenchmarkInfo', ['name', 'path', 'exit_code'])
+BenchmarkInfo = namedtuple('BenchmarkInfo', ['name', 'path', 'exit_code', 'color'])
 def _get_benchmark_info(benchmark_path):
     with open(str(benchmark_path / 'benchmark_exit_code.txt'), 'r') as in_f:
         exit_code = int(in_f.read())
-    return BenchmarkInfo(name=benchmark_path.name, path=benchmark_path, exit_code=exit_code)
+    color = 'black' if exit_code == 0 else 'red'
+    return BenchmarkInfo(name=benchmark_path.name, path=benchmark_path, exit_code=exit_code, color=color)
 
 
 if __name__ == '__main__':
     import sys
     root_path = Path(sys.argv[1])
     [travis_build_number, travis_build_url, travis_job_number, travis_job_url] = sys.argv[2:]
-    benchmarks = [ _get_benchmark_info(p) for p in root_path.iterdir() if p.is_dir() ]
+    rmw_impls = [ p for p in root_path.iterdir() if p.is_dir() ]
+    rwm_benchmarks = [
+      (rmw_impl.name, [ _get_benchmark_info(p) for p in rmw_impl.iterdir() if p.is_dir() ])
+        for rmw_impl in rmw_impls 
+    ]
     index_path = root_path / 'index.html'
     with open(str(index_path), 'w') as out_f:
         rendered_index = _results_index_template.render(
           result_id=root_path.name,
-          benchmarks=benchmarks,
-          plots=[ p.name for p in (benchmarks[0].path / 'plots').iterdir() if p.is_file() ],
+          rwm_benchmarks=rwm_benchmarks,
+          plots=[ p.name for p in (rwm_benchmarks[0][1][0].path / 'plots').iterdir() if p.is_file() ],
           plot_width=_plot_width,
           travis_build_number=travis_build_number,
           travis_build_url=travis_build_url,
